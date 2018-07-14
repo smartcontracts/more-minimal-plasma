3b6c35267bb5f88bad51f2dfea52da500dd9faa0
Kelvin Fichter <kelvinfichter@gmail.com>
2018-07-14 14:45:52 -0400

    Added more structure

--- contracts/RootChain.sol
+++ contracts/RootChain.sol
@@ -7,10 +7,59 @@ pragma solidity ^0.4.0;
  */
 contract RootChain {
     /*
+     * Events
+     */
+
+    event DepositCreated(
+        address indexed owner,
+        uint256 amount,
+        uint256 depositBlock
+    );
+
+    event BlockSubmitted(
+        bytes32 root
+    );
+
+    event ExitStarted(
+        address indexed owner,
+        uint256 utxoPosition,
+        uint256 amount
+    );
+
+
+    /*
      * Storage
      */
 
+    uint256 constant public EXIT_BOND = 123456789;
+
     address public operator;
+
+    mapping (uint256 => PlasmaBlock) public plasmaBlocks;
+    mapping (uint256 => PlasmaExit) public plasmaExits;
+
+    struct PlasmaBlock {
+        bytes32 root;
+    }
+
+    struct PlasmaExit {
+        address owner;
+        uint256 amount;
+    }
+
+    /*
+     * Modifiers
+     */
+
+    modifier onlyOperator() {
+        require(msg.sender == operator);
+        _;
+    }
+
+    modifier onlyWithValue(uint256 value) {
+        require(msg.value == value);
+        _;
+    }
 
     
     /*
@@ -19,5 +68,42 @@ contract RootChain {
 
     constructor() public {
         operator = msg.sender;
+    }
+
+
+    /*
+     * Public functions
+     */
+    
+    function deposit(bytes _encodedDepositTx) public {
+
+    }
+
+    function submitBlock(bytes32 _root) public onlyOperator {
+
+    }
+
+    function startExit(
+        uint256 _utxoPosition,
+        bytes _encodedTx,
+        bytes _txInclusionProof,
+        bytes _txSignatures
+    ) public onlyWithValue(EXIT_BOND) {
+
+    }
+
+    function challengeExit(
+        uint256 _exitingUtxoPosition,
+        uint256 _spendingTxPosition,
+        uint256 _encodedSpendingTx,
+        uint256 _spendingTxInclusionProof,
+        uint256 _spendingTxSignatures,
+        uint256 _spendingTxConfirmationSignatures
+    ) public {
+
+    }
+
+    function processExits() public {
+        
     }
 }
