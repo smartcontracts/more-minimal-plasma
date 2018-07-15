494fc81430c59d3395b880e587b1ff2d4891d946
Kelvin Fichter <kelvinfichter@gmail.com>
2018-07-15 19:10:31 -0400

    Simplified PriorityQueue

--- plasma/contracts/PriorityQueue.sol
+++ plasma/contracts/PriorityQueue.sol
@@ -26,8 +26,13 @@ contract PriorityQueue {
      */
 
     address owner;
-    uint256[] heapList;
+    QueueItem[] heapList;
     uint256 public currentSize;
+
+    struct QueueItem {
+        uint256 priority;
+        uint256 value;
+    }
 
 
     /*
@@ -36,7 +41,10 @@ contract PriorityQueue {
 
     constructor() public {
         owner = msg.sender;
-        heapList = [0];
+        heapList.push(QueueItem({
+            priority: 0,
+            value: 0
+        }));
         currentSize = 0;
     }
 
@@ -45,27 +53,30 @@ contract PriorityQueue {
      * Public functions
      */
 
-    function insert(uint256 k) public onlyOwner {
-        heapList.push(k);
+    function insert(uint256 priority, uint256 value) public onlyOwner {
+        heapList.push(QueueItem({
+            priority: priority,
+            value: value
+        }));
         currentSize = currentSize.add(1);
         percUp(currentSize);
     }
 
     function delMin() public onlyOwner returns (uint256) {
-        uint256 retVal = heapList[1];
-        heapList[1] = heapList[currentSize];
+        QueueItem memory retItem = heapList[1];
+        replaceItem(1, heapList[currentSize]);
         delete heapList[currentSize];
         currentSize = currentSize.sub(1);
         percDown(1);
         heapList.length = heapList.length.sub(1);
-        return retVal;
+        return retItem.value;
     }
 
     function minChild(uint256 i) public view returns (uint256) {
         if (i.mul(2).add(1) > currentSize) {
             return i.mul(2);
         } else {
-            if (heapList[i.mul(2)] < heapList[i.mul(2).add(1)]) {
+            if (heapList[i.mul(2)].priority < heapList[i.mul(2).add(1)].priority) {
                 return i.mul(2);
             } else {
                 return i.mul(2).add(1);
@@ -74,7 +85,7 @@ contract PriorityQueue {
     }
 
     function getMin() public view returns (uint256) {
-        return heapList[1];
+        return heapList[1].value;
     }
 
 
@@ -85,24 +96,35 @@ contract PriorityQueue {
     function percUp(uint256 _i) private {
         uint256 i = _i;
         uint256 j = i;
-        uint256 newVal = heapList[i];
-        while (newVal < heapList[i.div(2)]) {
-            heapList[i] = heapList[i.div(2)];
+        QueueItem memory newVal = heapList[i];
+        while (newVal.priority < heapList[i.div(2)].priority) {
+            replaceItem(i, heapList[i.div(2)]);
             i = i.div(2);
         }
-        if (i != j) heapList[i] = newVal;
+        if (i != j) {
+            replaceItem(i, newVal);
+        }
     }
 
     function percDown(uint256 _i) private {
         uint256 i = _i;
         uint256 j = i;
-        uint256 newVal = heapList[i];
+        QueueItem memory newVal = heapList[i];
         uint256 mc = minChild(i);
-        while (mc <= currentSize && newVal > heapList[mc]) {
-            heapList[i] = heapList[mc];
+        while (mc <= currentSize && newVal.priority > heapList[mc].priority) {
+            replaceItem(i, heapList[mc]);
             i = mc;
             mc = minChild(i);
         }
-        if (i != j) heapList[i] = newVal;
+        if (i != j) {
+            replaceItem(i, newVal);
+        }
+    }
+    
+    function replaceItem(uint256 _i, QueueItem memory _item) private {
+        heapList[_i] = QueueItem({
+            priority: _item.priority,
+            value: _item.value
+        });
     }
 }
