f16b9c604df50327f214984eb3bd62dc6f69ef2a
Kelvin Fichter <kelvinfichter@gmail.com>
2018-07-16 18:56:23 -0400

    Made priority queue more efficient

--- plasma/contracts/PriorityQueue.sol
+++ plasma/contracts/PriorityQueue.sol
@@ -10,6 +10,14 @@ import "./SafeMath.sol";
 contract PriorityQueue {
     using SafeMath for uint256;
 
+    /* 
+     *  Storage
+     */
+
+    address owner;
+    uint256[] heapList;
+    uint256 public currentSize;
+
 
     /*
      *  Modifiers
@@ -22,70 +30,52 @@ contract PriorityQueue {
 
 
     /*
-     *  Storage
-     */
-
-    address owner;
-    QueueItem[] heapList;
-    uint256 public currentSize;
-
-    struct QueueItem {
-        uint256 priority;
-        uint256 value;
-    }
-
-
-    /*
      * Constructor
      */
 
     constructor() public {
         owner = msg.sender;
-        heapList.push(QueueItem({
-            priority: 0,
-            value: 0
-        }));
+        heapList = [0];
         currentSize = 0;
     }
 
 
     /*
-     * Public functions
+     * Internal functions
      */
 
-    function insert(uint256 priority, uint256 value) public onlyOwner {
-        heapList.push(QueueItem({
-            priority: priority,
-            value: value
-        }));
+    /**
+     * @dev Inserts an element into the priority queue.
+     * @param _priority Priority to insert.
+     * @param _value Some additional value.
+     */
+    function insert(uint256 _priority, uint256 _value) public onlyOwner {
+        uint256 element = _priority << 128 | _value;
+        heapList.push(element);
         currentSize = currentSize.add(1);
-        percUp(currentSize);
+        _percUp(currentSize);
     }
 
+    /**
+     * @dev Returns the top element of the heap.
+     * @return The smallest element in the priority queue.
+     */
+    function getMin() public view returns (uint256, uint256) {
+        return _splitElement(heapList[1]);
+    }
+
+    /**
+     * @dev Deletes the top element of the heap and shifts everything up.
+     * @return The smallest element in the priorty queue.
+     */
     function delMin() public onlyOwner returns (uint256, uint256) {
-        QueueItem memory retItem = heapList[1];
-        replaceItem(1, heapList[currentSize]);
+        uint256 retVal = heapList[1];
+        heapList[1] = heapList[currentSize];
         delete heapList[currentSize];
         currentSize = currentSize.sub(1);
-        percDown(1);
+        _percDown(1);
         heapList.length = heapList.length.sub(1);
-        return (retItem.priority, retItem.value);
-    }
-
-    function minChild(uint256 i) public view returns (uint256) {
-        if (i.mul(2).add(1) > currentSize) {
-            return i.mul(2);
-        } else {
-            if (heapList[i.mul(2)].priority < heapList[i.mul(2).add(1)].priority) {
-                return i.mul(2);
-            } else {
-                return i.mul(2).add(1);
-            }
-        }
-    }
-
-    function getMin() public view returns (uint256, uint256) {
-        return (heapList[1].priority, heapList[1].value);
+        return _splitElement(retVal);
     }
 
 
@@ -93,38 +83,61 @@ contract PriorityQueue {
      * Private functions
      */
 
-    function percUp(uint256 _i) private {
-        uint256 i = _i;
-        uint256 j = i;
-        QueueItem memory newVal = heapList[i];
-        while (newVal.priority < heapList[i.div(2)].priority) {
-            replaceItem(i, heapList[i.div(2)]);
-            i = i.div(2);
-        }
-        if (i != j) {
-            replaceItem(i, newVal);
-        }
-    }
-
-    function percDown(uint256 _i) private {
-        uint256 i = _i;
-        uint256 j = i;
-        QueueItem memory newVal = heapList[i];
-        uint256 mc = minChild(i);
-        while (mc <= currentSize && newVal.priority > heapList[mc].priority) {
-            replaceItem(i, heapList[mc]);
-            i = mc;
-            mc = minChild(i);
-        }
-        if (i != j) {
-            replaceItem(i, newVal);
+    /**
+     * @dev Determines the minimum child of a given node in the tree.
+     * @param _index Index of the node in the tree.
+     * @return The smallest child node.
+     */
+    function _minChild(uint256 _index) private view returns (uint256) {
+        if (_index.mul(2).add(1) > currentSize) {
+            return _index.mul(2);
+        } else {
+            if (heapList[_index.mul(2)] < heapList[_index.mul(2).add(1)]) {
+                return _index.mul(2);
+            } else {
+                return _index.mul(2).add(1);
+            }
         }
     }
 
-    function replaceItem(uint256 _i, QueueItem memory _item) private {
-        heapList[_i] = QueueItem({
-            priority: _item.priority,
-            value: _item.value
-        });
+    /**
+     * @dev Bubbles the element at some index up.
+     */
+    function _percUp(uint256 _index) private {
+        uint256 index = _index;
+        uint256 j = index;
+        uint256 newVal = heapList[index];
+        while (newVal < heapList[index.div(2)]) {
+            heapList[index] = heapList[index.div(2)];
+            index = index.div(2);
+        }
+        if (index != j) heapList[index] = newVal;
+    }
+
+    /**
+     * @dev Bubbles the element at some index down.
+     */
+    function _percDown(uint256 _index) private {
+        uint256 index = _index;
+        uint256 j = index;
+        uint256 newVal = heapList[index];
+        uint256 mc = _minChild(index);
+        while (mc <= currentSize && newVal > heapList[mc]) {
+            heapList[index] = heapList[mc];
+            index = mc;
+            mc = _minChild(index);
+        }
+        if (index != j) heapList[index] = newVal;
+    }
+
+    /**
+     * @dev Split an element into its priority and value.
+     * @param _element Element to decode.
+     * @return A tuple containing the priority and value.
+     */
+    function _splitElement(uint256 _element) private pure returns (uint256, uint256) {
+        uint256 priority = _element >> 128;
+        uint256 value = uint256(uint128(_element));
+        return (priority, value);
     }
 }
